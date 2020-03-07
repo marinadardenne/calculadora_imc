597201e0e3f9278aedaeb85e582d520229ed42b5
marinadardenne <marina.mdardenne@gmail.com>
2020-03-07 18:27:41 -0300

    git commit

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Calculadora de IMC"),
+        centerTitle: true,
+        backgroundColor: Colors.red[300],
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: () {},
+          )
+        ],
+      ),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Icon(Icons.person_outline, size: 120.0, color: Colors.red[300]),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: "Peso (kg)",
+                  labelStyle: TextStyle(color: Colors.red[300])),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.red[300], fontSize: 25.0),
+            ),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: "Altura (m)",
+                  labelStyle: TextStyle(color: Colors.red[300])),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.red[300], fontSize: 25.0),
+            ),
+            Container(
+              height: 50.0 ,
+              child: RaisedButton(
+                onPressed:() {},
+                child: Text("Calcular", 
+                  style: TextStyle(color: Colors.white, fontSize: 25.0),),
+                color: Colors.red[300],
+              ),
+            ),
+            Text("Info",
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.red[300], fontSize: 25.0,),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
